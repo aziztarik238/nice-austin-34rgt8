7ac9a92641ef89beb1f2ee60d512b210335f4896
Md. Tarik Aziz <69706577+aziztarik238@users.noreply.github.com>
2023-03-04 20:41:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+
+      <div class="accordion">
+  <div class="accordion-item">
+    <div class="accordion-header">
+      <h3>Question 1</h3>
+      <i class="fas fa-chevron-down"></i>
+    </div>
+    <div class="accordion-body">
+      <p>Answer to Question 1</p>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <div class="accordion-header">
+      <h3>Question 2</h3>
+      <i class="fas fa-chevron-down"></i>
+    </div>
+    <div class="accordion-body">
+      <p>Answer to Question 2</p>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <div class="accordion-header">
+      <h3>Question 3</h3>
+      <i class="fas fa-chevron-down"></i>
+    </div>
+    <div class="accordion-body">
+      <p>Answer to Question 3</p>
+    </div>
+  </div>
+</div>
+
+    </div>
+  );
+}
